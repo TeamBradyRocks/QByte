7e6eccf1a824a22d3e22d088584108e5d8f8ae86
Aswin Sivaraman <sivaram2@illinois.edu>
2014-06-21 00:37:07 -0700

    Got white bar, logo placeholder, and cafe tabs

--- Web/index.php
+++ Web/index.php
@@ -1,12 +1,49 @@
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no" />
 <head>
 <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
-<h1>I am formatted with an external style sheet</h1>
-<p>Me too!</p>
+	<div class="header">
+		<img width="100px" height="48px" src="" alt="Logo here" />
+	</div>
+	<div class="main">
+		<h1>Cafe AA</h1>
+			<div id="menu-1">
+			  <ul class='tabs'>
+				<li><a href='#tab1'>Mon</a></li>
+				<li><a href='#tab2'>Tue</a></li>
+				<li><a href='#tab3'>Wed</a></li>
+				<li><a href='#tab4'>Thu</a></li>
+				<li><a href='#tab5'>Fri</a></li>
+			  </ul>
+			  <div id='menu1-tab1'>
+				<p>Hi, this is the first tab.</p>
+			  </div>
+			  <div id='menu1-tab2'>
+				<p>This is the 2nd tab.</p>
+			  </div>
+			  <div id='menu1-tab3'>
+				<p>And this is the 3rd tab.</p>
+			  </div>
+			  <div id='menu1-tab4'>
+				<p>And this is the 3rd tab.</p>
+			  </div>
+			  <div id='menu1-tab5'>
+				<p>And this is the 3rd tab.</p>
+			  </div>
+			  <div id='tab3'>
+				<p>And this is the 3rd tab.</p>
+			  </div>
+			</div>
+		<h1>Cafe Q</h1>
+		<h1>Cafe R</h1>
+		<h1>Cafe S</h1>
+		<h1>Cafe WT</h1>
+		Test.
+	</div>
 </body>
 
 </html>
